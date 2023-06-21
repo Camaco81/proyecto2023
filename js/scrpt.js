f22747bd4d99d82f7501e134e377b53df3e4f40c
David Reyes <reyisacc@gmail.com>
2023-06-21 11:55:25 -0400

    ediciones

--- js/scrpt.js
+++ js/scrpt.js
@@ -1,4 +1,7 @@
 
+//saber la distancia del header desde el top del documento
+const header = document.getElementById("main-header");
+var sticky =header.offsetTop;
 
 // When the user scrolls down 50px from the top of the document, resize the header's font size
 // cuando el scroll del usuario baja 50px del top de el documento se rediseña ell header
@@ -8,12 +11,12 @@ window.onscroll = function () { scrollFunction() };
 function scrollFunction() {// detecta el cambio del scroll
 
     //capturo los que voy a modificar
-    const header = document.getElementById("main-header");
+    //ya tengo capturado el header en la linea 3
     const menuItem = document.querySelectorAll(".menuItem");
     const logo = document.getElementById("logo");
 
     // si pasa de 50px 
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    if (document.body.scrollTop > sticky || document.documentElement.scrollTop > sticky) {
         //reduce el padding
         header.style.padding = "5px 0px";
         //cambia el tamaño de la letra
@@ -22,6 +25,9 @@ function scrollFunction() {// detecta el cambio del scroll
         })
         //resize logo
         logo.style.height="40px";
+
+            //agregar clase sticky (fijar header dinamicamente)
+    header.classList.add("sticky");
     } else {
         //sino los valores default
         header.style.padding = "20px 0px";
@@ -30,5 +36,8 @@ function scrollFunction() {// detecta el cambio del scroll
             e.style.fontSize = "20px";
         })
         logo.style.height="50px";
+
+        header.classList.remove("sticky");
     }
+
 }
